ac2d30ab1f8376627e88113cfd116e794cafddcf
Franklin marek <franklinmarek@mac.attlocal.net>
2026-04-25 15:20:07 -0500

    Add SASpro cask and formula V1.15.4

---
+++ Casks/saspro.rb
@@ -0,0 +1,27 @@
+cask "saspro" do
+  version "1.15.4"
+
+  on_arm do
+    url "https://github.com/setiastro/setiastrosuitepro/releases/download/V#{version}/SetiAstroSuitePro_AppleSilicon.dmg"
+    sha256 "f2319fdd364a691e31d5c9ba875e7e0eb6d684fe7ede6e8b08064427d7604872"  # paste full hash here
+  end
+
+  on_intel do
+    url "https://github.com/setiastro/setiastrosuitepro/releases/download/V#{version}/SetiAstroSuitePro_macOS_Intel.dmg"
+    sha256 "f451d5c14d8353db1c3db91986e0d23ca5b400acb6f75f845f56f490c3fb026d"  # paste full hash here
+  end
+
+  name "SASpro"
+  desc "SetiAstro Suite Pro astrophotography image processing platform"
+  homepage "https://setiastro.com"
+
+  depends_on macos: ">= :ventura"
+
+  app "SASpro.app"
+
+  zap trash: [
+    "~/Library/Application Support/SASpro",
+    "~/Library/Preferences/com.setiastro.saspro.plist",
+    "~/Library/Caches/SASpro",
+  ]
+end
---
+++ Formula/saspro.rb
@@ -0,0 +1,19 @@
+class Saspro < Formula
+  desc "SetiAstro Suite Pro astrophotography image processing platform"
+  homepage "https://setiastro.com"
+  url "https://github.com/setiastro/setiastrosuitepro/archive/refs/tags/V1.15.4.tar.gz"
+  sha256 "22ba8faaaf1bb6a25193ee458edc357d97acb30f7b87e79fb2faa103c8cb4c5f"
+  license "Nonstandard"
+
+  depends_on "python@3.12"
+
+  def install
+    venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install_and_link(buildpath)
+    bin.write_exec_script libexec/"bin/saspro"
+  end
+
+  test do
+    system bin/"saspro", "--version"
+  end
+end
