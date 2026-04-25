7d8e42fe6f13dad5cba10e09a526200d38ba57ff
Franklin marek <franklinmarek@mac.attlocal.net>
2026-04-25 18:02:04 -0500

    trying simpler formula

--- Formula/saspro.rb
+++ Formula/saspro.rb
@@ -7,22 +7,11 @@ class Saspro < Formula
   license "GPL-3.0-only"
 
   depends_on "python@3.12"
-  depends_on "tbb"
 
   def install
     system libexec/"bin/python3", "-m", "pip", "install",
-           "--upgrade", "--quiet", "pip"
-    system libexec/"bin/python3", "-m", "pip", "install",
            "--quiet", "--no-warn-script-location", "setiastrosuitepro"
-
-    # Wrapper script that sets safe Numba threading before launch
-    (bin/"setiastrosuitepro").write <<~EOS
-      #!/bin/bash
-      export NUMBA_THREADING_LAYER=omp
-      export NUMBA_NUM_THREADS=1
-      exec "#{libexec}/bin/setiastrosuitepro" "$@"
-    EOS
-    chmod 0755, bin/"setiastrosuitepro"
+    bin.install_symlink libexec/"bin/setiastrosuitepro"
   end
 
   test do
