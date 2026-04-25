15a47e31da6785ca402023a037a7a7f8d3d8c93b
Franklin marek <franklinmarek@mac.attlocal.net>
2026-04-25 16:07:12 -0500

    license

--- Formula/saspro.rb
+++ Formula/saspro.rb
@@ -3,7 +3,7 @@ class Saspro < Formula
   homepage "https://setiastro.com"
   url "https://github.com/setiastro/setiastrosuitepro/archive/refs/tags/V1.15.4.tar.gz"
   sha256 "22ba8faaaf1bb6a25193ee458edc357d97acb30f7b87e79fb2faa103c8cb4c5f"
-  license "Nonstandard"
+  license "GPL-3.0-only"
 
   depends_on "python@3.12"
 
