cc81b487e5b8e2fcf8725f7e04876509468b0e74
Franklin Marek <frank@setiastro.com>
2026-07-21 12:53:22 -0500

    1.19.10.post2

--- Casks/saspro.rb
+++ Casks/saspro.rb
@@ -3,12 +3,12 @@ cask "saspro" do
 
   on_arm do
     url "https://github.com/setiastro/setiastrosuitepro/releases/download/V#{version}/SetiAstroSuitePro_AppleSilicon.dmg"
-    sha256 "c885ad6613a025af7974a02f4a1f7e0e0b8731741b937c0172a75df4ca48d6fa"  # paste full hash here
+    sha256 "c488c982c4ce7f6f73ab98a68b4ab235b0c02fc44309ce1c9d3040678a92f631"  # paste full hash here
   end
 
   on_intel do
     url "https://github.com/setiastro/setiastrosuitepro/releases/download/V#{version}/SetiAstroSuitePro_macOS_Intel.dmg"
-    sha256 "3ee643cea431e23c7ca49087cb787bac87634819b0a94bae4e84fb9192bef0c9"  # paste full hash here
+    sha256 "9c29176d5c2456ab722e5244103f5ac7e217519dab4f971c0b254f7e9ad18d50"  # paste full hash here
   end
 
   name "SASpro"
