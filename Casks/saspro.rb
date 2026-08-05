4aae7e2bd866064870d354ed7efb272922347cd2
Franklin Marek <frank@setiastro.com>
2026-08-05 07:56:16 -0500

    1.20.0

--- Casks/saspro.rb
+++ Casks/saspro.rb
@@ -1,14 +1,14 @@
 cask "saspro" do
-  version "1.19.18"
+  version "1.20.0"
 
   on_arm do
     url "https://github.com/setiastro/setiastrosuitepro/releases/download/V#{version}/SetiAstroSuitePro_AppleSilicon.dmg"
-    sha256 "64c1131d121fa6f508074507186ab559441ab023f6d350643fbc0fc4ac66d73c"  # paste full hash here
+    sha256 "b503dfbfa630caa712c5d00b7606404e50eb2097b81e6bc8611a94cc1cb19747"  # paste full hash here
   end
 
   on_intel do
     url "https://github.com/setiastro/setiastrosuitepro/releases/download/V#{version}/SetiAstroSuitePro_macOS_Intel.dmg"
-    sha256 "c9ac5c0d80cdd550c5ede237093a6fdecb521e59350889e3875f74bb1fc173b5"  # paste full hash here
+    sha256 "f496d6d921521bb6ab7a699fbabe63c726171162ebd54324a9f8586ea72c73ef"  # paste full hash here
   end
 
   name "SASpro"
--- Formula/saspro.rb
+++ Formula/saspro.rb
@@ -2,8 +2,8 @@ class Saspro < Formula
   include Language::Python::Virtualenv
   desc "SetiAstro Suite Pro astrophotography image processing platform"
   homepage "https://setiastro.com"
-  url "https://github.com/setiastro/setiastrosuitepro/archive/refs/tags/V1.19.18.tar.gz"
-  sha256 "067dc1629bc9976348b1b75120a9cbfef475f884f033fc9190af44c6ceae087a"
+  url "https://github.com/setiastro/setiastrosuitepro/archive/refs/tags/V1.20.0.tar.gz"
+  sha256 "fd77b9ed18b5343effab07f1ea350bb3a53df0d1f4ad9de56aea94b83a7ed5d4"
   license "GPL-3.0-only"
 
   depends_on "python@3.12" => :recommended
